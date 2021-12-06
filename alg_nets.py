e5d7602d759bbe51853a17e4a6d95ab38a7839d0
Arseni <1919ars@gmail.com>
2021-12-06 12:04:54 +0200

    (pro)

--- alg_nets.py
+++ alg_nets.py
@@ -14,9 +14,9 @@ class ActorNet(nn.Module):
 
         self.net = nn.Sequential(
             nn.Linear(obs_size, 64),
-            nn.ReLU(),
+            nn.ELU(),
             nn.Linear(64, 64),
-            nn.ReLU(),
+            nn.ELU(),
             nn.Linear(64, n_actions),
             # nn.Tanh()
             nn.Sigmoid(),
@@ -27,11 +27,7 @@ class ActorNet(nn.Module):
         self.entropy_term = 0
 
     def forward(self, state):
-        if type(state) is np.ndarray:
-            state = Variable(torch.from_numpy(state).float().unsqueeze(0))
-        state = state.float()
         value = self.net(state)
-
         return value
 
 
@@ -41,7 +37,7 @@ class CriticNet(nn.Module):
     n_actions: number of discrete actions available in the environment
     # hidden_size: size of hidden layers
     """
-    def __init__(self, obs_size: int, n_actions: int, n_agents: int):
+    def __init__(self, obs_size: int, n_actions: int, n_agents: int=1):
         super(CriticNet, self).__init__()
 
         self.obs_net = nn.Sequential(
@@ -49,33 +45,15 @@ class CriticNet(nn.Module):
             nn.ReLU(),
             nn.Linear(64, 64),
             nn.ReLU(),
-            nn.Linear(64, obs_size * n_agents),
-            nn.ReLU(),
-        )
-
-        self.out_net = nn.Sequential(
-            nn.Linear(obs_size * n_agents + n_actions * n_agents, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
             nn.Linear(64, 1),
+            nn.ReLU(),
         )
 
         self.n_actions = n_actions
         self.obs_size = obs_size
         self.n_agents = n_agents
-        self.entropy_term = 0
 
-    def forward(self, state, action):
-        if type(state) is not torch.Tensor:
-            # state = Variable(torch.from_numpy(state).float().unsqueeze(0))
-            state = Variable(torch.tensor(state, requires_grad=True).float().unsqueeze(0))
-            action = Variable(torch.tensor(action, requires_grad=True).float().unsqueeze(0))
-
-        state = state.float()
-        obs = self.obs_net(state)
-        cat = torch.cat([obs, action], dim=1)
-        value = self.out_net(cat)
-
+    def forward(self, state):
+        value = self.obs_net(state)
         return value
 
